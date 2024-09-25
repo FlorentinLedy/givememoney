559ea86bdd3be0b46792478506dfae0288be96f4
Florentin Ledy <26773259+FlorentinLedy@users.noreply.github.com>
2024-09-25 15:30:41 +0200

    bug

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,9 +1,9 @@
 import unittest
-from givememoneyjulien.main import give_me_money_julien
+from give_me_money_pastaga.main import give_me_money_pastaga
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        compte_en_banque = give_me_money_julien(8, 10)
+        compte_en_banque = give_me_money_pastaga(8, 10)
         self.assertEqual(compte_en_banque, 18)
         
 if __name__ == '__main__':
